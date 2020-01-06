413958c5465ee4583d96907a2a33ceebcaf28bf4
Michiel de Jong <michiel@unhosted.org>
2020-01-06 13:40:15 +0100

    step debugging

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // Set up the view for the subject indicated in the fragment of the window's URL
   const uri = decodeURIComponent(window.location.hash.substr(1))
   if (uri.length === 0) {
-    window.location = '?#' + encodeURIComponent('https://michielbdejong.inrupt.net/meetings/Data%20browser%2016%20Dec%202019/Chat/chat.ttl#this')
+    window.location = '?#' + encodeURIComponent('https://w3c-cg.solid.community/public/w3c-cg-chat/index.ttl#this')
   }
   appendChatPane(document, uri)
 })
