337995551e7d36eeb33de8f43ddf971a72a24e0f
Michiel de Jong <michiel@unhosted.org>
2020-08-14 14:03:13 +0200

    Add invites list, invite sender, and chats list to stand-alone version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* global SolidAuthClient */
+
 const ChatPane = require('./longChatPane.js')
 const $rdf = require('rdflib')
 const UI = require('solid-ui')
@@ -5,6 +7,72 @@ const SolidAuth = require('solid-auth-client')
 
 // FIXME:
 window.$rdf = $rdf
+window.followLink = async function (from, follow, multiple) {
+  const subject = $rdf.sym(from)
+  const doc = subject.doc()
+  await new Promise((resolve, reject) => {
+    UI.store.fetcher.load(doc).then(resolve, reject)
+  })
+  const predicate = $rdf.sym(follow)
+  if (multiple) {
+    return UI.store.each(subject, predicate).map(n => n.value)
+  }
+  return UI.store.any(subject, predicate).value
+}
+
+function toLi (uri) {
+  return `<li><a href="?#${encodeURIComponent(uri)}">${uri}</a></li>`
+}
+
+async function getChatsList () {
+  const { instances } = await UI.authn.findAppInstances({}, $rdf.sym('http://www.w3.org/ns/pim/meeting#LongChat'))
+  return `<h2>Your chats:
+<ul>
+  ${instances.map(n => n.value).map(toLi)}
+</ul>`
+}
+
+window.sendInvite = async function () {
+  const invitee = document.getElementById('invitee').value
+  const inviteeInbox = await window.followLink(invitee, UI.ns.ldp('inbox'))
+  const body = `
+<> a <http://www.w3.org/ns/pim/meeting#LongChatInvite> ;
+  ${UI.ns.rdf('seeAlso')} <${decodeURIComponent(window.location.hash.substr(1))}> .
+`
+  fetch(inviteeInbox, {
+    method: 'POST',
+    body,
+    headers: {
+      'Content-Type': 'text/turtle'
+    }
+  })
+}
+
+async function getInvitesList () {
+  const webId = (await SolidAuthClient.currentSession()).webId
+  const globalInbox = await window.followLink(webId, UI.ns.ldp('inbox'))
+  const inboxItems = await window.followLink(globalInbox, UI.ns.ldp('contains'), true)
+  const invites = []
+  const promises = inboxItems.map(async x => {
+    try {
+      const inboxMsgTypes = await window.followLink(x, UI.ns.rdf('type'), true)
+      const isLongChatInvite = (inboxMsgTypes.indexOf('http://www.w3.org/ns/pim/meeting#LongChatInvite') !== -1)
+      if (isLongChatInvite) {
+        console.log('new chat!', x)
+        const chatUrl = await window.followLink(x, UI.ns.rdf('seeAlso'))
+        invites.push(chatUrl)
+      }
+    } catch (e) {
+      console.error('Failed to parse inbox item', x)
+    }
+  })
+  await Promise.all(promises)
+  return `<h2>Your Invites:
+  <ul>
+    ${invites.map(toLi)}
+  </ul>
+  <input id="invitee"><button onclick="sendInvite()">Send Invite</button>`
+}
 
 async function appendChatPane (dom, uri) {
   const subject = $rdf.sym(uri)
@@ -21,6 +89,13 @@ async function appendChatPane (dom, uri) {
   }
   const options = {}
 
+  const menuDiv = document.createElement('div')
+  console.log('waiting 2 seconds')
+  await (new Promise(resolve => setTimeout(resolve, 2000)))
+  console.log('waited 2 seconds')
+  menuDiv.innerHTML = await getInvitesList()
+  menuDiv.innerHTML += await getChatsList()
+  dom.body.appendChild(menuDiv)
   const paneDiv = ChatPane.render(subject, context, options)
   dom.body.appendChild(paneDiv)
 }
