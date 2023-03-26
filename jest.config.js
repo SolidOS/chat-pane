3b634a3d6ec7817d8b9dc719f0b429fa165a7d9d
Sharon Stratsianis <SharonStrats@users.noreply.github.com>
2023-03-26 11:18:27 -0700

    Update jest.config.js
    Co-authored-by: Alain Bourgeois <alain.bourgeois10@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ["./jest.setup.ts"],
   transformIgnorePatterns: ["/node_modules/(?!lit-html).+\\.js"],
+  testEnvironmentOptions: {
+      customExportConditions: ['node']
+  }
 };
