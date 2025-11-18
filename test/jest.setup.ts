b8fb80b8f08b9a1ed8b805000752103edc313304
timea-solid <4144203+timea-solid@users.noreply.github.com>
2025-11-18 13:37:01 +0100

    fix a lint issue

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom'
 import fetchMock from 'jest-fetch-mock'
 import { TextEncoder, TextDecoder } from 'util'
 
-global.TextEncoder = TextEncoder;
+global.TextEncoder = TextEncoder
 global.TextDecoder = TextDecoder
 
 fetchMock.enableMocks()
