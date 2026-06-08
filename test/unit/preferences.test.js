abaa4bfc8170894d684cdc34b6318799534ee88e
timea-solid <4144203+timea-solid@users.noreply.github.com>
2026-06-08 10:49:01 +0200

    moved over test for renderPreferencesForm

---
+++ test/unit/preferences.test.js
@@ -0,0 +1,22 @@
+import { sym } from 'rdflib'
+import * as UI from 'solid-ui'
+import { renderPreferencesForm } from '../../src/preferences'
+import { JSDOM } from 'jsdom'
+
+const window = new JSDOM('<!DOCTYPE html><p>Hello world</p>').window
+const dom = window.document
+
+describe('renderPreferencesForm', () => {
+  it('exists', () => {
+    expect(renderPreferencesForm).toBeInstanceOf(Function)
+  })
+  it('runs', () => {
+    const subject = sym('https://test.test')
+    const theClass = {}
+    const preferencesForm = {}
+    const context = { dom }
+    expect(renderPreferencesForm(
+      subject, theClass, preferencesForm, context
+    )).toBeTruthy()
+  })
+})
